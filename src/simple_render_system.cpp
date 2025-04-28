39d954537d4524a356a79ad42c1baec180518bb8
Simon Descôteaux <simde771@gmail.com>
2025-04-28 15:05:01 -0400

    Start of 3d, new transform

--- src/simple_render_system.cpp
+++ src/simple_render_system.cpp
@@ -13,8 +13,7 @@
 namespace lve
 {
     struct SinglePushConstantData{
-        glm::mat2 transform{1.f};
-        glm::vec2 offset;
+        glm::mat4 transform{1.f};
         alignas(16) glm::vec3 color;
     };
 
@@ -71,12 +70,11 @@ namespace lve
 
         for (auto &obj:gameObjects)
         {
-            obj.transform2d.rotation = glm::mod(obj.transform2d.rotation + 0.01f, glm::two_pi<float>());
+            obj.transform.rotation = glm::mod(obj.transform.rotation + 0.005f, glm::two_pi<float>());
 
             SinglePushConstantData push{};
-            push.offset = obj.transform2d.translation;
             push.color = obj.color;
-            push.transform = obj.transform2d.mat2();
+            push.transform = obj.transform.mat4();
 
             vkCmdPushConstants(commandBuffer, pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 0, sizeof(SinglePushConstantData), &push);
 
