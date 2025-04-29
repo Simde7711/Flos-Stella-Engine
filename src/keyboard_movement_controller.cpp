f322f4a135136022bf476b3b93f2ca37cc7a1ae2
Simon Descôteaux <simde771@gmail.com>
2025-04-29 16:08:53 -0400

    mouvement comme dans unity

--- src/keyboard_movement_controller.cpp
+++ src/keyboard_movement_controller.cpp
@@ -1,4 +1,5 @@
 #include "keyboard_movement_controller.hpp"
+#include "gtc/quaternion.hpp"
 
 namespace lve
 {
@@ -30,10 +31,10 @@ namespace lve
         gameObject.transform.rotation.x = glm::clamp(gameObject.transform.rotation.x, -1.5f, 1.5f);
         gameObject.transform.rotation.y = glm::mod(gameObject.transform.rotation.y, glm::two_pi<float>());
 
-        float yaw = gameObject.transform.rotation.y;
-        const glm::vec3 forwardDir{sin(yaw), 0.f, cos(yaw)};
-        const glm::vec3 rightDir{forwardDir.z, 0.f, -forwardDir.x};
-        const glm::vec3 upDir{0.f, -1.f, 0.f};
+        // Base local directions
+        const glm::vec3 forwardDir = glm::vec3(0.f, 0.f, 1.f);
+        const glm::vec3 rightDir   = glm::vec3(1.f, 0.f,  0.f);
+        const glm::vec3 upDir      = glm::vec3(0.f, 1.f,  0.f);
 
         glm::vec3 moveDir{0.f};
         if (glfwGetKey(window, keys.moveForward) == GLFW_PRESS)
@@ -63,7 +64,12 @@ namespace lve
 
         if (glm::dot(moveDir, moveDir) > std::numeric_limits<float>::epsilon())
         {
-            gameObject.transform.translation += moveSpeed * deltaTime * glm::normalize(moveDir);
+            moveDir = glm::normalize(moveDir);
+
+            glm::quat rotQuat = glm::quat(gameObject.transform.rotation); // if in radians
+            glm::vec3 worldDir = rotQuat * moveDir;
+
+            gameObject.transform.translation += moveSpeed * deltaTime * worldDir;
         }
     }
 }
